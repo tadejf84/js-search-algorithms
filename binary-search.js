81c5271aac6fb6198d56e5aabeb8e999fd07f86e
tadej <tadej@mail.com>
2019-02-12 21:03:45 +0100

    binary search algorithm

--- binary-search.js
+++ binary-search.js
@@ -1,33 +1,67 @@
+/*
+* binary search for sorted array
+* select a midpoint in the array and compare with the data your are searching for
+* if larger move to the right half of the array, else to the left half
+* continue until finding the match
+* complexity of the algorithm - O(log n)
+*/
 function binarySearch (arr, data) {
 
     // sort array first
-    arr = arr.filter( (a, b) => {
-        return a - b
-    });
-
-    console.log(arr);
+    arr.sort((a, b) => a - b);
 
     // define bounds
     let start = 0, end = arr.length - 1;
 
     while (start <= end) {
-        let mid = Math.floor((end + start) / 2);
+			
+				// set midpoint
+        const mid = Math.floor((end + start) / 2);
+			
         if (arr[mid] < data) {
             start = mid + 1;
         } else if (arr[mid] > data) {
             end = mid - 1;
         } else {
-            return true;
+            return mid;
         }
+			
     }
-    return false;
+    return -1;
 }
 
+
 /*
-* helper function to sort array
+* count occurences of the data
 */
-function sortArr(arr) {
-    return arr.filter( (a,b) => {
-        return a - b
-    });
+function binCount(arr, data) {	
+	let count = 0;
+	
+	// get position of data
+	const pos = binarySearch(arr, data);
+	
+	// if data is found continue, else return 0
+	if (pos !== -1) {
+		count++;
+		
+		// search to the left for occurences of data
+		for (let i = pos - 1; i >= 0; --i) {
+			if (arr[i] === data) {
+				count++;
+			} else {
+				break;
+			}
+		}
+		
+		// search to the right for occurences of data
+		for (let i = pos + 1; i < arr.length; ++i) {
+			if (arr[i] === data) {
+				count++;
+			} else {
+				break;
+			}
+		}
+	}
+
+	return count;
 }
