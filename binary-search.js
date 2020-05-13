9c80608fc54e0b292510104dd8143d1f0a60b80a
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-05-13 12:37:40 +0200

    binary search algorithm

--- binary-search.js
+++ binary-search.js
@@ -1,21 +1,25 @@
-/*
-* binary search for sorted array
-* select a midpoint in the array and compare with the data your are searching for
-* if larger move to the right half of the array, else to the left half
-* continue until finding the match
-* complexity of the algorithm - O(log n)
-*/
-function binarySearch (arr, data) {
+/**
+ * Binary search for sorted array
+ * Select a midpoint in the array and compare with the input data, 
+ * If larger move to the right half of the array, else to the left half
+ * Continue until finding the match
+ * 
+ * @param {array} arr 
+ * @param {number} data 
+ * 
+ * @returns {number} position of data in array (-1 if not found)
+ */
+function binarySearch(arr, data) {
 
-    // sort array first
+    // Sort array first
     arr.sort((a, b) => a - b);
 
-    // define bounds
+    // Define bounds
     let start = 0, end = arr.length - 1;
 
     while (start <= end) {
 			
-				// set midpoint
+		// Set midpoint
         const mid = Math.floor((end + start) / 2);
 			
         if (arr[mid] < data) {
@@ -23,43 +27,40 @@ function binarySearch (arr, data) {
         } else if (arr[mid] > data) {
             end = mid - 1;
         } else {
-            return mid;
-        }
-			
-    }
-    return -1;
+            return mid; // Found position
+		}
+		
+	}
+	
+    return -1; // Not found
 }
 
 
-/*
-* count occurences of the data
-*/
-function binCount(arr, data) {	
+/**
+ * Count occurences of the data
+ * 
+ * @param {array} arr 
+ * @param {number} data 
+ * 
+ * @returns {number} count occurences
+ */
+function binaryCount(arr, data) {	
 	let count = 0;
 	
-	// get position of data
+	// Get position of data
 	const pos = binarySearch(arr, data);
 	
-	// if data is found continue, else return 0
 	if (pos !== -1) {
 		count++;
 		
-		// search to the left for occurences of data
+		// Search to the left for occurences of data
 		for (let i = pos - 1; i >= 0; --i) {
-			if (arr[i] === data) {
-				count++;
-			} else {
-				break;
-			}
+			if (arr[i] === data) count++;
 		}
 		
-		// search to the right for occurences of data
+		// Search to the right for occurences of data
 		for (let i = pos + 1; i < arr.length; ++i) {
-			if (arr[i] === data) {
-				count++;
-			} else {
-				break;
-			}
+			if (arr[i] === data) count++;
 		}
 	}
 
